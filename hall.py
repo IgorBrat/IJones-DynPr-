e7c7f63cdc0b6d983a826701dbbd6340379cf21a
IgorBrat <akbars5678@gmail.com>
2022-10-29 18:57:37 +0300

    Finally did dfs with memoization!!! (reduced 33k variants to 422☺)

--- hall.py
+++ hall.py
@@ -6,7 +6,7 @@ class Hall:
         with open(filein) as file:
             hall = file.read().splitlines()
         dimensions = hall[0].split(" ")
-        self.width, self.height  = int(dimensions[0]), int(dimensions[1])
+        self.width, self.height = int(dimensions[0]), int(dimensions[1])
         self.hall = []
         for i in range(1, self.height + 1):
             row = []
@@ -19,7 +19,8 @@ class Hall:
         else:
             self.out = [(self.height - 1, self.width - 1)]
         print("Out: " + str(self.out))
-        print("="*60)
+        print("=" * 60)
+        self.explored_paths = {}
 
     def get_candidates(self, position: tuple):
         row, column = position
@@ -35,17 +36,40 @@ class Hall:
         while node.parent is not None:
             node = node.parent
             path.append(node)
-        return list(reversed(path))
+        return path
 
     def dfs_modified(self, start: tuple):
+        paths_count = 0
         if start[0] not in range(self.height) or start[1] not in range(self.width):
             raise IndexError("Element is out of bonds of the maze")
         stack = []
         start = Node(self.hall[start[0]][start[1]], start)
         stack.append(start)
         while len(stack) != 0:
-            node = stack.pop(0)
-            if node.position in self.out:
-                print("Path: " + str(self.__get_path(node)))
-            for position in self.get_candidates(node.position):
-                stack.append(Node(self.hall[position[0]][position[1]], position, node))
+            node = stack.pop()
+            if node.position in self.explored_paths.keys():
+                curr_pos_paths_count = self.explored_paths[node.position]
+                curr_path = self.__get_path(node)
+                paths_count += 1
+                print("Path: " + str(curr_path))
+                for parent_node in curr_path:
+                    if parent_node.position != node.position:
+                        if parent_node.position in self.explored_paths.keys():
+                            self.explored_paths[parent_node.position] += curr_pos_paths_count
+                        else:
+                            self.explored_paths[parent_node.position] = curr_pos_paths_count
+            elif node.position in self.out:
+                curr_path = self.__get_path(node)
+                print("Path: " + str(curr_path))
+                paths_count += 1
+                for curr_node in curr_path[1:]:
+                    if curr_node.position not in self.explored_paths.keys():
+                        self.explored_paths[curr_node.position] = 1
+                    else:
+                        self.explored_paths[curr_node.position] += 1
+            else:
+                for position in self.get_candidates(node.position):
+                    candidate_node = Node(self.hall[position[0]][position[1]], position, node)
+                    stack.append(candidate_node)
+        print(paths_count)
+        print(self.explored_paths)
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from ijones.hall import Hall
 
 
 def main():
-    hall = Hall("resources/ijones.in")
-    hall.dfs_modified((1, 0))
+    hall = Hall("resources/ijones2.in")
+    hall.dfs_modified((0, 0))
 
 
 if __name__ == '__main__':
