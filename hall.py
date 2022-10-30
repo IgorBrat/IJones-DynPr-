ac4e7c810c820651408dc0ac3362ca6aa85a4330
IgorBrat <akbars5678@gmail.com>
2022-10-30 12:47:36 +0200

    Finished project

--- hall.py
+++ hall.py
@@ -3,6 +3,7 @@ from ijones.node import Node
 
 class Hall:
     def __init__(self, filein: str):
+        self.fileout = filein.split(".")[0] + ".out"
         with open(filein) as file:
             hall = file.read().splitlines()
         dimensions = hall[0].split(" ")
@@ -13,16 +14,13 @@ class Hall:
             for j in range(self.width):
                 row.append(hall[i][j])
             self.hall.append(row)
-        print("Hall: " + str(self.hall))
         if self.height > 1:
             self.out = [(0, self.width - 1), (self.height - 1, self.width - 1)]
         else:
             self.out = [(self.height - 1, self.width - 1)]
-        print("Out: " + str(self.out))
-        print("=" * 60)
         self.explored_paths = {}
 
-    def get_candidates(self, position: tuple):
+    def __get_candidates(self, position: tuple):
         row, column = position
         candidates = []
         for i in range(self.height):
@@ -31,15 +29,15 @@ class Hall:
                     candidates.append((i, j))
         return candidates
 
-    def __get_path(self, node: Node):
+    @staticmethod
+    def __get_path(node: Node):
         path = [node]
         while node.parent is not None:
             node = node.parent
             path.append(node)
         return path
 
-    def dfs_modified(self, start: tuple):
-        paths_count = 0
+    def __dfs_modified(self, start: tuple):
         if start[0] not in range(self.height) or start[1] not in range(self.width):
             raise IndexError("Element is out of bonds of the maze")
         stack = []
@@ -50,8 +48,6 @@ class Hall:
             if node.position in self.explored_paths.keys():
                 curr_pos_paths_count = self.explored_paths[node.position]
                 curr_path = self.__get_path(node)
-                paths_count += 1
-                print("Path: " + str(curr_path))
                 for parent_node in curr_path:
                     if parent_node.position != node.position:
                         if parent_node.position in self.explored_paths.keys():
@@ -60,16 +56,27 @@ class Hall:
                             self.explored_paths[parent_node.position] = curr_pos_paths_count
             elif node.position in self.out:
                 curr_path = self.__get_path(node)
-                print("Path: " + str(curr_path))
-                paths_count += 1
                 for curr_node in curr_path[1:]:
                     if curr_node.position not in self.explored_paths.keys():
                         self.explored_paths[curr_node.position] = 1
                     else:
                         self.explored_paths[curr_node.position] += 1
             else:
-                for position in self.get_candidates(node.position):
+                for position in self.__get_candidates(node.position):
                     candidate_node = Node(self.hall[position[0]][position[1]], position, node)
                     stack.append(candidate_node)
-        print(paths_count)
-        print(self.explored_paths)
+        return self.explored_paths[(start.position[0], start.position[1])]
+
+    def get_all_unique_paths(self):
+        print("Started application")
+        print("=" * 60)
+        print("Processing...\n")
+        all_unique_paths = 0
+        for row in range(self.height):
+            all_unique_paths += self.__dfs_modified((row, 0))
+        self.__write_res_combinations(all_unique_paths)
+        print(f"Successfully written result to {self.fileout}\n\n\n")
+
+    def __write_res_combinations(self, all_unique_paths: int):
+        with open(self.fileout, 'w') as fileout:
+            fileout.write("All unique paths: " + str(all_unique_paths))
--- main.py
+++ main.py
@@ -2,8 +2,27 @@ from ijones.hall import Hall
 
 
 def main():
-    hall = Hall("resources/ijones2.in")
-    hall.dfs_modified((0, 0))
+    path = "resources/"
+
+    # Initial hall
+
+    hall = Hall(path + "ijones.in")
+    hall.get_all_unique_paths()
+
+    # Hall 1
+
+    hall1 = Hall(path + "ijones1.in")
+    hall1.get_all_unique_paths()
+
+    # Hall 2
+
+    hall2 = Hall(path + "ijones2.in")
+    hall2.get_all_unique_paths()
+
+    # Hall 3
+
+    hall3 = Hall(path + "ijones3.in")
+    hall3.get_all_unique_paths()
 
 
 if __name__ == '__main__':
