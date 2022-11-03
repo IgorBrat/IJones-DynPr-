b79281d0f5c59af23773d20ecd4e23e794530346
IgorBrat <akbars5678@gmail.com>
2022-11-03 23:15:33 +0200

    Added bottom-up implementation

--- hall.py
+++ hall.py
@@ -1,8 +1,7 @@
-from ijones.node import Node
-
-
 class Hall:
     def __init__(self, filein: str):
+        print(f"\n\nStarted application for {filein}")
+        print("="*30)
         self.fileout = filein.split(".")[0] + ".out"
         with open(filein) as file:
             hall = file.read().splitlines()
@@ -14,68 +13,39 @@ class Hall:
             for j in range(self.width):
                 row.append(hall[i][j])
             self.hall.append(row)
-        if self.height > 1:
-            self.out = [(0, self.width - 1), (self.height - 1, self.width - 1)]
+        self.dp = [[0 for i in range(self.width)] for j in range(self.height)]
+
+    def bottom_up(self, row, col):
+        if self.dp[row][col]:
+            res = self.dp[row][col]
         else:
-            self.out = [(self.height - 1, self.width - 1)]
-        self.explored_paths = {}
+            res = 0
+            if col != self.width - 1:
+                for i in range(self.height - 1, -1, -1):
+                    for j in range(self.width - 1, col, -1):
+                        if self.hall[i][j] == self.hall[row][col] or (i == row and j == col + 1):
+                            res += self.dp[i][j]
+        return res
 
-    def __get_candidates(self, position: tuple):
-        row, column = position
-        candidates = []
+    def get_paths(self, out_row, out_col):
+        self.dp[out_row][out_col] = 1
+        res = [0 for j in range(self.height)]
+        for col in range(self.width - 1, -1, -1):
+            for row in range(self.height - 1, -1, -1):
+                self.dp[row][col] = self.bottom_up(row, col)
         for i in range(self.height):
-            for j in range(column + 1, self.width):
-                if (i == row and j == column + 1) or (self.hall[row][column] == self.hall[i][j]):
-                    candidates.append((i, j))
-        return candidates
+            res[i] += self.dp[i][0]
+        return sum(res)
 
-    @staticmethod
-    def __get_path(node: Node):
-        path = [node]
-        while node.parent is not None:
-            node = node.parent
-            path.append(node)
-        return path
-
-    def __dfs_modified(self, start: tuple):
-        if start[0] not in range(self.height) or start[1] not in range(self.width):
-            raise IndexError("Element is out of bonds of the maze")
-        stack = []
-        start = Node(self.hall[start[0]][start[1]], start)
-        stack.append(start)
-        while len(stack) != 0:
-            node = stack.pop()
-            if node.position in self.explored_paths.keys():
-                curr_pos_paths_count = self.explored_paths[node.position]
-                curr_path = self.__get_path(node)
-                for parent_node in curr_path:
-                    if parent_node.position != node.position:
-                        if parent_node.position in self.explored_paths.keys():
-                            self.explored_paths[parent_node.position] += curr_pos_paths_count
-                        else:
-                            self.explored_paths[parent_node.position] = curr_pos_paths_count
-            elif node.position in self.out:
-                curr_path = self.__get_path(node)
-                for curr_node in curr_path[1:]:
-                    if curr_node.position not in self.explored_paths.keys():
-                        self.explored_paths[curr_node.position] = 1
-                    else:
-                        self.explored_paths[curr_node.position] += 1
-            else:
-                for position in self.__get_candidates(node.position):
-                    candidate_node = Node(self.hall[position[0]][position[1]], position, node)
-                    stack.append(candidate_node)
-        return self.explored_paths[(start.position[0], start.position[1])]
-
-    def get_all_unique_paths(self):
-        print("Started application")
-        print("=" * 60)
-        print("Processing...\n")
-        all_unique_paths = 0
-        for row in range(self.height):
-            all_unique_paths += self.__dfs_modified((row, 0))
-        self.__write_res_combinations(all_unique_paths)
-        print(f"Successfully written result to {self.fileout}\n\n\n")
+    def find_all_paths(self):
+        if self.height != 1:
+            res = self.get_paths(self.height - 1, self.width - 1)
+            self.dp = [[0 for i in range(self.width)] for j in range(self.height)]
+            res += self.get_paths(0, self.width - 1)
+        else:
+            res = self.get_paths(0, self.width - 1)
+        self.__write_res_combinations(res)
+        print("Finished calculating")
 
     def __write_res_combinations(self, all_unique_paths: int):
         with open(self.fileout, 'w') as fileout:
--- main.py
+++ main.py
@@ -7,22 +7,22 @@ def main():
     # Initial hall
 
     hall = Hall(path + "ijones.in")
-    hall.get_all_unique_paths()
+    hall.find_all_paths()
 
     # Hall 1
 
     hall1 = Hall(path + "ijones1.in")
-    hall1.get_all_unique_paths()
+    hall1.find_all_paths()
 
     # Hall 2
 
     hall2 = Hall(path + "ijones2.in")
-    hall2.get_all_unique_paths()
+    hall2.find_all_paths()
 
     # Hall 3
 
     hall3 = Hall(path + "ijones3.in")
-    hall3.get_all_unique_paths()
+    hall3.find_all_paths()
 
 
 if __name__ == '__main__':
